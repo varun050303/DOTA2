c73f205d09700d523bab835d0e643c65f184270d
varun singhal <varunsinghal3655@gmail.com>
2024-03-11 22:37:23 +0530

    Dota is working

--- js/hero.js
+++ js/hero.js
@@ -1,4 +1,4 @@
-const heroName = 'nevermore'
+const heroName = 'abaddon'
 const dotaApi = 'https://api.opendota.com/api'
 const heroNameEl = document.querySelector('[data-hero-name]')
 const heroImageEl = document.querySelector('[data-hero-image]')
@@ -24,43 +24,44 @@ fetch(`${dotaApi}/constants/heroes`)
         heroImageEl.src = `${hero.image}`;
     })
 
-fetch(`${dotaApi}/constants/abilities`)
-    .then(response => response.json())
-    .then(body => {
-        const allAbility = body
-        fetch(`${dotaApi}/constants/hero_abilities`).then(response => response.json())
-            .then(body => {
-                const heroAbilities = body[`npc_dota_hero_${heroName}`].abilities
-                    .filter(ability => ability !== 'generic_hidden')
-                    .map(ability => allAbility[ability])
-                    .map(ability => {
-                        return {
-                            name: ability.dname,
-                            description: ability.desc,
-                            image: `https://raw.githubusercontent.com/navix/dota2-database/master/images/abilities/${ability.img.replace("/apps/dota2/images/dota_react/abilities/", "").replace(".png", "_hp2.png")}`
-                        }
-                    })
-                    .map(ability => {
-                        return `<li class="ability">
-                        <p class="ability__title">${ability.name}</p>
-                        <img class="ability__img" src="${ability.image}" alt="${ability.name}" />
-                        <p class="ability__description">${ability.description}</p>
-                        </li>`
-                    })
-                    .join('')
 
-                heroAbilitiesEl.innerHTML = heroAbilities
-            })
-    })
+Promise.all([
+    fetch(`${dotaApi}/constants/abilities`).then(response => response.json()),
+    fetch(`${dotaApi}/constants/hero_abilities`).then(response => response.json())]
+).then(body => {
+    const allAbility = body[0]
+    const heroesAbilities = body[1]
+    const heroAbilities = heroesAbilities[`npc_dota_hero_${heroName}`].abilities
+        .filter(ability => ability !== 'generic_hidden')
+        .map(ability => allAbility[ability])
+        .map(ability => {
+            return {
+                name: ability.dname,
+                description: ability.desc,
+                image: `https://raw.githubusercontent.com/navix/dota2-database/master/images/abilities/${ability.img.replace("/apps/dota2/images/dota_react/abilities/", "").replace(".png", "_hp1.png")}`
+            }
+        })
+        .map(ability => {
+            return `<li class="ability">
+        <p class="ability__title">${ability.name}</p>
+        <img class="ability__img" src="${ability.image}" alt="${ability.name}" />
+        <p class="ability__description">${ability.description}</p>
+        </li>`
+        })
+        .join('')
+    heroAbilitiesEl.innerHTML = heroAbilities
+    heroAbilitiesEl.closest('section').removeAttribute('hidden')
+})
 
 fetch(`${dotaApi}/constants/hero_lore`).then(response => response.json())
     .then(body => {
         const heroLore = body[heroName]
         heroDescEl.textContent = heroLore
     })
+
+
 function capitalize(word) {
     return word.slice(0, 1).toUpperCase() + word.slice(1)
 }
 
 heroNameEl.textContent = capitalize(heroName)
-
